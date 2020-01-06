80c8df839bae37fd6fd15f5a71226725130f5e23
thienphuc1 <phuc.dam@student.passerellesnumeriques.org>
2020-01-07 04:09:58 +0700

    main screen and search

---
+++ src/component/Book.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import {View, Image, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+class Book extends Component {
+  render() {
+    return (
+      <View style={styles.showflast}>
+        <TouchableOpacity>
+          <Image
+            source={{uri: this.props.image}}
+            style={{width: 150, height: 200, backgroundColor: 'red'}}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.name}>{this.props.name}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.author}>{this.props.author}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.rate}>
+          <Icon name="ios-star" size={11} color="#fc9619" />
+          <Icon name="ios-star" size={11} color="#fc9619" />
+          <Icon name="ios-star" size={11} color="#fc9619" />
+          <Icon name="ios-star" size={11} color="#fc9619" />
+          <Icon name="ios-star" size={11} color="#979797" />
+          <Text style={styles.bookCount}>{this.props.count}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  bookCount: {
+    color: '#ababab',
+    paddingLeft: 10,
+  },
+  topbar: {
+    paddingLeft: 15,
+    paddingTop: 20.5,
+    fontSize: 10.5,
+    flexDirection: 'row',
+    //color: 'red',
+    //flex: 1,
+  },
+  search: {
+    marginLeft: 230,
+  },
+  list: {
+    paddingTop: 5,
+  },
+  text: {
+    fontSize: 25.5,
+    paddingTop: 5,
+  },
+  main: {
+    padding: 10,
+  },
+  rate: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  showflast: {
+    padding: 5,
+    width: 150,
+  },
+  name: {
+    color: '#4a4a4a',
+    fontSize: 18,
+  },
+  author: {
+    color: '#ababab',
+    fontSize: 16,
+  },
+});
+export default Book;
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Navigation} from 'react-native-navigation';
 import App from '../../App';
 import Home from '../screens/Home';
+import Book from '../component/Book';
+import Search from '../component/Search';
 
 function ReduxProvider(Component) {
   return props => (
@@ -16,15 +18,25 @@ export default () => {
     Navigation.setRoot({
       root: {
         component: {
-          name: 'Home',
+          name: 'Search',
         },
       },
     });
   });
 
   Navigation.registerComponent(
+    'Search',
+    () => ReduxProvider(Search),
+    () => Search,
+  );
+  Navigation.registerComponent(
     'Home',
     () => ReduxProvider(Home),
     () => Home,
+  );
+  Navigation.registerComponent(
+    'Book',
+    () => ReduxProvider(Book),
+    () => Book,
   );
 };
